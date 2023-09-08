ff40dfcb5ec9b27e6cf5cc6893c804770783b623
jwangSD22 <jackw1689@gmail.com>
2023-09-08 11:24:21 -0700

    bfsdfs

--- easy/0144redo.js
+++ easy/0144redo.js
@@ -1,7 +1,9 @@
-//144. BINARY TREE PREORDER TRAVERSAL
-
 /**
  * Definition for a binary tree node.
+ * 
+ * 
+ * CAN YOU DO THIS DFS AND BFS?
+ * 
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.left = (left===undefined ? null : left)
@@ -13,15 +15,7 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
+    
 
-    
-    preorderTraversal(this.left)
-    console.log(this.val)
-    preorderTraversal(this.right)
-    
-    
+
 };
-
-console.log(Number.isInteger(5))
-
-console.log(Math.min(5,3))
