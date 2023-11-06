e207fdee10d6f6f72e692e1ea000d5e176e64279
jack-ubuntu <jackw1689@gmail.com>
2023-11-06 11:10:20 -0800

    0017

--- medium/0017xxx.js
+++ medium/0017xxx.js
@@ -1,5 +1,45 @@
-let myarray = [1,2,3]
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
 
-console.log(myarray.slice(0,3))
+    if(!digits){
+        return []
+    }
+    
+let hash = 
+{
+    '2':'abc',
+    '3':'def',
+    '4':'ghi',
+    '5':'jkl',
+    '6':'mno',
+    '7':'pqrs',
+    '8':'tuv',
+    '9':'wxyz'
+}
 
+let final = ['']
+
+for (let digit of digits){
+    let newCombos = []
+    for(let combo of final){
+    
+        for(let letter of hash[digit]){
+            newCombos.push(combo+letter)
+        }
+
+    }
+
+    final = newCombos
+}
+
+return final
+
+
+
+};
+
+//this is iterative approach, can you do backtracking approach?
 
