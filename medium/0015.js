285df5d5725c67efd7641871137963848bff2412
jack-windows <jackw1689@gmail.com>
2023-12-12 21:45:22 -0800

    threesum

--- medium/0015.js
+++ medium/0015.js
@@ -3,15 +3,32 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    const res=[]
+    nums.sort((a,b)=>a-b)
+    for(let i=0;i<nums.length;i++){
+        //we skip the numbers where the current and the previous are the same because the first line of the while loop will sum the first two numbers together, and we don't want repeat sets 
+        if(i>0 && nums[i] == nums[i-1]){
+            continue;
+        }
+        let left=i+1;
+        let right = nums.length-1;
 
-    let output = []
+        while(left<right){
+            let threesum = nums[i]+nums[left]+nums[right]
+            if(threesum>0){
+                right--;
+            }else if(threesum<0){
+                left++;
+            }else{
+                res.push([nums[i],nums[left],nums[right]])
+                left++;
 
-
-
-    // can't do this in one pass because there are multiple variants of triplets we need to keep track of
-    // do we have to use dynamic programming?
-
-
-    
-    
+                // this line increments left because we dont want to capture the same issue as the comment under the for loop
+                while(nums[left] == nums[left-1] && left<right){
+                    left++;
+                }
+            }
+        }
+    }
+    return res
 };
--- medium/0128.js
+++ medium/0128.js
@@ -5,29 +5,37 @@
 var longestConsecutive = function(nums) {
 
 let ans = new Set(nums)
-console.log(ans)
-
-let count = 0 
 let max = 0
 
 for(let num of nums){
-    count=1
-    temp = num
-    while(ans.has(temp+1)){
+    let count=1
+    while(ans.has(++num)){
         count++
-        temp++
     }
-    if(count>max){
-        max=count
-    }
-
-    
+    max = Math.max(max,count)
+   
 }
 
-return Math.max(count,max)
+return max
     
     
 };
 
 
-console.log(longestConsecutive(nums))
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const set = new Set(nums);
+    let longestStreak = 0;
+
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            let currentStreak = 1;
+            while (set.has(++num)) currentStreak++;
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+    return longestStreak;
+};
