dcf0ab183d622a2cfaca2b49a2c5e29f593d2126
jack-windows <jackw1689@gmail.com>
2023-12-14 22:22:24 -0800

    219

--- easy/0219.js
+++ easy/0219.js
@@ -7,54 +7,18 @@ var containsNearbyDuplicate = function(nums, k) {
 
     let h = new Map()
 
-    for(let i = 0 ; i<nums.length;i++){
+for(let i = 0 ; i<nums.length-1; i ++ ){
 
-        if(!h.has(nums[i])){
-            h.set(nums[i],[i])
-        }
-        else{
-            h.set(nums[i],[...h.get(nums[i]),i])
-        }
-
-
-
+    if(i-h.get(nums[i])<=k){
+        return true
     }
-
-    for(let [key,v] of h){
-
-        if(v.length>1){
-
-            let low = v[0]
-
-            let diff =  0
     
-            for(let i = 1 ;i<v.length;i++){
-    
-                diff = v[i]-low
-    
-                if(diff>k){
-                    low=v[i]
-                }
-                else if(diff<=k){
-                    return true
-                }
-        
-            }
-        }
+    h.set(nums[i],i)
 
 
-
-    }
-
-
-
+}
 
     return false
 
 
 };
-
-let nums = [1,2,3,1]
-
-console.log(containsNearbyDuplicate(nums,3))
-
