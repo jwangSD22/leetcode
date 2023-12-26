074572d7dafac216de39ef1409c5ab51feabdb00
jack-windows <jackw1689@gmail.com>
2023-12-26 00:44:26 -0800

    find min

--- medium/0153retry.js
+++ medium/0153retry.js
@@ -2,18 +2,36 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-    if(nums[0]<nums[nums.length-1]){
-        return nums[0]
+var findMin = function (nums) {
+
+    let lo = 0
+    let hi = nums.length - 1
+
+
+
+    while (lo<hi) {
+
+        let mid = (lo + hi) >> 1
+
+        // if mid is greater than hi, then the lowest value is in right half
+
+        if (nums[mid] > nums[hi]) {
+            lo = mid+1
+        }
+        else {
+            hi = mid
+
+        }
+        // if lo is greater than mid, then the lowest value is in the left half
+        // but you must check and include mid 
+
     }
 
-
+    return nums[lo]
 
 
 
 
 };
-let array = [1,2,3,4,5,6,7,8,9]
 
-
-console.log(array.slice(array.length/2))
+console.log(findMin([2,3,4,5,6,7,8,9]))
