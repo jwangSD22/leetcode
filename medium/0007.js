7236b0e2845144a12eba27c4b5d4d6b9901d65ae
jack-windows <jackw1689@gmail.com>
2023-12-25 19:49:10 -0800

    reverse digits

--- medium/0007.js
+++ medium/0007.js
@@ -4,42 +4,22 @@
  */
 var reverse = function(x) {
 
-    let string = x.toString()
-    let negative = false
+    let final = 0 
 
-
-    if(string[0]==='-'){
-        negative = true
+    while(x>0){
+        let a = x%10
+        x = Math.trunc(x/10)
+        final=final*10+a
     }
 
-    if(negative){
-        string=string.slice(1)
-    }
-
-    let number = string.split('')
-
-    if(number.length>31){
-        return 0
-    }
-
-    while(number[number.length-1]==='0'){
-        number.pop()
-    }
-
-
-
-    if(negative){
-        number.push('-')
-    }
-
-    let final = number.reverse().join('')
-
-
-    return final>(2**31)-1||final<(-1*2**31)?0:final
-
-
+    
+return final
 
     
 };
 
-console.log(reverse(1534236469))
+
+
+console.log(reverse(123456))
+
+
--- medium/0017xxx.js
+++ medium/0017xxx.js
@@ -2,44 +2,32 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
-
-    if(!digits){
+var letterCombinations = function (digits) {
+    if (digits.length === 0) {
         return []
     }
-    
-let hash = 
-{
-    '2':'abc',
-    '3':'def',
-    '4':'ghi',
-    '5':'jkl',
-    '6':'mno',
-    '7':'pqrs',
-    '8':'tuv',
-    '9':'wxyz'
-}
+    let final = []
+    let h = { 2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wxyz' }
+    helper(final, '', digits)
 
-let final = ['']
+    return final
 
-for (let digit of digits){
-    let newCombos = []
-    for(let combo of final){
-    
-        for(let letter of hash[digit]){
-            newCombos.push(combo+letter)
+
+    function helper(array, current, digits) {
+        if (digits.length === 0) {
+            array.push(current)
+            return
         }
-
+        if (digits[0] > 1) {
+            for (let letter of h[digits[0]]) {
+                helper(array, current + letter, digits.slice(1))
+            }
+        }
+        else {
+            helper(array, current, digits.slice(1))
+        }
     }
-
-    final = newCombos
-}
-
-return final
-
-
 
 };
 
-//this is iterative approach, can you do backtracking approach?
-
+console.log(letterCombinations('23'))
